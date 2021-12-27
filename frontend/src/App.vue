4561be95fd26f63007d9930308322a7bdd43ad5a
Tenzin Thabkhae <tenzin.thabkhae@mail.utoronto.ca>
2021-12-26 20:36:02 -0500

    updates

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,36 +1,37 @@
 <script lang="ts">
-import { ref } from 'vue'
+import { ref, reactive } from "vue";
 
 export default {
   setup() {
-    const todoList = ref([]);
-    const newTodo = ref('');
+    const todoList = reactive([]);
+    const newTodo = ref("");
     // list.value = []
 
     function appendList(newText: string) {
-      if (newText === '')
-      return
-      todoList.value.push({ 'name': newText })
+      if (newText === "") return;
+      todoList.push({ name: newText });
+      console.log("------------");
+      console.log(todoList);
     }
 
-    return { todoList, newTodo, appendList}
-  }
-}
+    return { todoList, newTodo, appendList };
+  },
+};
 </script>
 
 <template>
-<input type="text" v-model=newTodo />
-<button @click="newTodo=''">Clear</button>
-<button @click="appendList(newTodo)">Add</button>
-<div>{{ todoList.values.length }}</div>
-<hr />
-<div>{{ newTodo }}</div>
-<hr />
-<div>{{ todoList }}</div>
-<hr />
-<ul>
-  <li v-for="todo in todoList">{{ todo.name }}</li>
-</ul>
+  <input type="text" v-model="newTodo" />
+  <button @click="newTodo = ''">Clear</button>
+  <button @click="appendList(newTodo)">Add</button>
+  <div>{{ todoList.length }}</div>
+  <hr />
+  <div>{{ newTodo }}</div>
+  <hr />
+  <div>{{ todoList }}</div>
+  <hr />
+  <ul>
+    <li v-for="todo in todoList">{{ todo.name }}</li>
+  </ul>
 </template>
 
 <style>
